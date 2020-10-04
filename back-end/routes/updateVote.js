32d8cc4d62646b374805bcb1f92a4aeca6b5083a
jairoMolina9 <molinaandres9991@gmail.com>
2020-10-04 01:29:13 -0400

    fixed

--- back-end/routes/updateVote.js
+++ back-end/routes/updateVote.js
@@ -6,16 +6,16 @@ router.post("/", function(req, res, next) {
   async function updateVote() {
     try {
       let doc = await db.get(req.body.classID);
-      var votes = doc.lectures[req.body.lectureID].notes[req.body.sectionID].votes[req.body.type];
+      var votes = doc.lectures[req.body.lectureID].notes[req.body.noteID].votes[req.body.sectionID];
 
       if(req.body.type == -1) { //downvote
         if(votes == 0) {
           return false;
         } else {
-          doc.lectures[req.body.lectureID].notes[req.body.sectionID].votes[req.body.type]--;
+          doc.lectures[req.body.lectureID].notes[req.body.noteID].votes[req.body.sectionID]--;
         }
       } else if (req.body.type == 1) { //upvote
-        doc.lectures[req.body.lectureID].notes[req.body.sectionID].votes[req.body.type]++;
+        doc.lectures[req.body.lectureID].notes[req.body.noteID].votes[req.body.sectionID]++;
       }
 
       let res = await db.insert(doc, doc._id);
