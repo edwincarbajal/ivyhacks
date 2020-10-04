dad964ca35581a2dd8f391fc43bfc7cca5bd8ade
edwincarbajal <exaviercarbajal@gmail.com>
2020-10-04 11:31:19 -0400

    fixup! 📝 Adds course and lecture title to editor and saves notes

--- back-end/routes/addContent.js
+++ back-end/routes/addContent.js
@@ -1,44 +1,64 @@
 var express = require("express");
 var router = express.Router();
-const { db } = require('../db');
+const { db } = require("../db");
 
-router.post("/", function(req, res, next) {
+function addSpaces(html) {
+  let result = [];
+  for (let i = html.length - 1; i >= 0; i--) {
+    if (html[i] == "<") {
+      result.push(html[i]);
+      result.push(" ");
+    } else if (html[i] == ">") {
+      result.push(" ");
+      result.push(html[i]);
+    } else {
+      result.push(html[i]);
+    }
+  }
+  return result.reverse().join("");
+}
+
+router.post("/", function (req, res, next) {
   async function updateVote() {
     try {
       let doc = await db.get(req.body.classID);
       var notes = doc.lectures[req.body.lectureID].notes[req.body.noteID];
 
-      var content = req.body.content;
+      let reqContent = req.body.content;
 
-      content = content + " <h2>"; //need this at the end
+      let result = addSpaces(reqContent);
 
-      var sections = content.match(/(?<=<h2>\s+).*?(?=\s+<\/h2>)/gs);
-      var content = content.match(/(?<=<\/h2>\s+).*?(?=\s+<h2>)/gs);
+      result = result + " <h2>"; //need this at the end
+      var sections = result.match(/(?<=<h2>\s+).*?(?=\s+<\/h2>)/gs);
+      var content = result.match(/(?<=<\/h2>\s+).*?(?=\s+<h2>)/gs);
 
-      for(var i = 0; i < sections.length; i++) {
-        doc.lectures[req.body.lectureID].notes[req.body.noteID].sections.push(sections[i]);
-        doc.lectures[req.body.lectureID].notes[req.body.noteID].content.push(content[i]);
+      for (var i = 0; i < sections.length; i++) {
+        doc.lectures[req.body.lectureID].notes[req.body.noteID].sections.push(
+          sections[i]
+        );
+        doc.lectures[req.body.lectureID].notes[req.body.noteID].content.push(
+          content[i]
+        );
         doc.lectures[req.body.lectureID].notes[req.body.noteID].votes.push(0);
       }
 
       let res = await db.insert(doc, doc._id);
 
       return true;
-
-    } catch(err){
+    } catch (err) {
       return false;
       console.error(`Error -> ${error}`);
     }
   }
 
-updateVote().then((data) => {
-  console.log("Note content updated");
-  res.send(data);
-}).catch((err) => {
-  res.send("Didnt get data");
-});
-
-
+  updateVote()
+    .then((data) => {
+      console.log("Note content updated");
+      res.send(data);
+    })
+    .catch((err) => {
+      res.send("Didnt get data");
+    });
 });
 
 module.exports = router;
