07778fdf51ee63d13443de837f4718449af9af5b
Jinay Jain <jinaybjain@gmail.com>
2020-10-04 12:56:43 -0400

    Add ant design to login/register

--- front-end/src/components/Register/index.js
+++ front-end/src/components/Register/index.js
@@ -1,101 +1,92 @@
+import { Form, Input, Button } from "antd";
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import fire from "../../firebase";
-import "./register.css";
 
 const RegisterForm = (props) => {
-  const [user, setUser] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+    const [user, setUser] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-  const [emailError, setEmailError] = useState("");
-  const [passwordError, setPasswordError] = useState("");
+    const [emailError, setEmailError] = useState("");
+    const [passwordError, setPasswordError] = useState("");
 
-  const clearInputs = () => {
-    setEmail("");
-    setPassword("");
-  };
+    const clearInputs = () => {
+        setEmail("");
+        setPassword("");
+    };
 
-  const clearErrors = () => {
-    setEmailError("");
-    setPasswordError("");
-  };
+    const clearErrors = () => {
+        setEmailError("");
+        setPasswordError("");
+    };
 
-  const handleSignup = () => {
-    clearErrors();
-    fire
-      .auth()
-      .createUserWithEmailAndPassword(email, password)
-      .catch((err) => {
-        switch (err.code) {
-          case "auth/email-already-in-use":
-          case "auth/invalid-email":
-            setEmailError(err.message);
-            break;
-          case "auth/weak-password":
-            setPasswordError(err.message);
-            break;
-        }
-      });
-  };
+    const handleSignup = (values) => {
+        clearErrors();
+        fire.auth()
+            .createUserWithEmailAndPassword(values.email, values.password)
+            .catch((err) => {
+                switch (err.code) {
+                    case "auth/email-already-in-use":
+                    case "auth/invalid-email":
+                        setEmailError(err.message);
+                        break;
+                    case "auth/weak-password":
+                        setPasswordError(err.message);
+                        break;
+                }
+            });
+    };
 
-  const authListener = () => {
-    fire.auth().onAuthStateChanged((user) => {
-      if (user) {
-        clearInputs();
-        setUser(user);
-      } else {
-        setUser("");
-      }
-    });
-  };
+    const authListener = () => {
+        fire.auth().onAuthStateChanged((user) => {
+            if (user) {
+                clearInputs();
+                setUser(user);
+            } else {
+                setUser("");
+            }
+        });
+    };
 
-  useEffect(() => {
-    authListener();
-  }, []);
+    useEffect(() => {
+        authListener();
+    }, []);
 
-  return (
-    <div className="LoginForm">
-      <label> Email </label>
-      <input
-        type="text"
-        autoFocus
-        required
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <p>{emailError}</p>
-      <label> Password </label>
-      <input
-        type="password"
-        required
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <p>{passwordError}</p>
-      <input
-        onClick={handleSignup}
-        className="submit-btn"
-        type="submit"
-        value="Register"
-      />
-    </div>
-  );
+    return (
+        <Form onFinish={(values) => handleSignup(values)}>
+            <h1>Sign up for Recall</h1>
+            <Form.Item
+                label="Email"
+                name="email"
+                rules={[{ required: true, message: "Please provide email" }]}
+            >
+                <Input type="email" />
+            </Form.Item>
+            <p>{emailError}</p>
+            <Form.Item
+                label="Password"
+                name="password"
+                rules={[{ required: true, message: "Please provide password" }]}
+            >
+                <Input type="password" />
+            </Form.Item>
+
+            <p>{passwordError}</p>
+            <Form.Item>
+                <Button type="primary" htmlType="submit" size="large" shape="round">Sign Up</Button>
+            </Form.Item>
+            <Link to="/login">Already have an account?</Link>
+        </Form>
+    );
 };
 
 function Register(props) {
-  return (
-    <div className="register">
-      <h1 className="title">Collab Notes - REGISTER</h1>
-      <RegisterForm />
-      <p className="prompt-create-user clickable">
-        {" "}
-        <Link className="link" to="/Login">
-          Already Have an Account?
-        </Link>
-      </p>
-    </div>
-  );
+    return (
+        <div className="auth-container">
+            <RegisterForm />
+        </div>
+    );
 }
 
 export default Register;
